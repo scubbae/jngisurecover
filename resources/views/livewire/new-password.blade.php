aa72036bee960a76acb01bd54f8efe55768a110c
Jerry Ellis <scubbae@gmail.com>
2023-08-15 18:26:04 -0500

    Update new-password.blade.php

--- resources/views/livewire/new-password.blade.php
+++ resources/views/livewire/new-password.blade.php
@@ -6,7 +6,7 @@
             <x-alerts />
             <form wire:submit.prevent="resetPassword">
                 <input class="form-control mb-3" type="text" wire:model="email" placeholder="Enter your email" readonly>
-                <input class="form-control mb-3" type="text" wire:model="token" placeholder="Enter reset token" readonly>
+                <input class="form-control mb-3" type="hidden" wire:model="token" placeholder="Enter reset token" readonly>
                 <input class="form-control mb-3" type="password" wire:model="password" placeholder="Enter new password">
                 <input class="form-control mb-3" type="password" wire:model="passwordConfirmation" placeholder="Confirm new password">
                 <button class="btn btn-primary w-100" type="submit">Save</button>
